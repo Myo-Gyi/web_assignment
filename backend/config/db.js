a483ef3c4e435ab60fae11a7dab00616a3fa9551
mkkz-dev <mkkz.dev@gmail.com>
2026-05-05 17:28:22 +0900

    edit db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -16,8 +16,18 @@ if (process.env.NODE_ENV === 'test') {
     process.env.DB_PASSWORD,
     {
       host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 3306,
       dialect: 'mysql',
       logging: false,
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+      dialectOptions: {
+        connectTimeout: 60000,
+      },
     }
   );
 }
